c62c2c563760edb9ecf769466030e5ce4a928843
edrysorg <edrys.org@pm.me>
2022-09-22 12:25:25 +1000

    Clena up dependencies

--- server/deps.ts
+++ server/deps.ts
@@ -1,15 +1,14 @@
-export * as oak from "https://deno.land/x/oak/mod.ts";
-// @deno-types="https://deno.land/x/otpauth/dist/otpauth.d.ts"
-export * as otpauth from "https://deno.land/x/otpauth/dist/otpauth.esm.js";
-export { SMTPClient } from "https://deno.land/x/denomailer/mod.ts";
 export * as log from "https://deno.land/std/log/mod.ts";
 export { crypto } from "https://deno.land/std/crypto/mod.ts";
-export * as djwt from "https://deno.land/x/djwt/mod.ts";
-export * as s3 from "https://deno.land/x/s3_lite_client@0.1.1/mod.ts";
-export * as s3_helpers from "https://deno.land/x/s3_lite_client@0.1.1/helpers.ts";
-export * as denodb from "https://deno.land/x/denodb/mod.ts";
 export * as base64 from "https://deno.land/std/encoding/base64.ts";
 export * as fs from "https://deno.land/std/fs/mod.ts";
-export { nanoid } from "https://deno.land/x/nanoid/mod.ts";
 export * as flags from "https://deno.land/std/flags/mod.ts";
 export * as path from "https://deno.land/std/path/mod.ts";
+
+export * as oak from "https://deno.land/x/oak/mod.ts";
+export { nanoid } from "https://deno.land/x/nanoid/mod.ts";
+export * as djwt from "https://deno.land/x/djwt/mod.ts";
+export * as s3 from "https://deno.land/x/s3_lite_client@0.1.1/mod.ts";
+// @deno-types="https://deno.land/x/otpauth/dist/otpauth.d.ts"
+export * as otpauth from "https://deno.land/x/otpauth/dist/otpauth.esm.js"; 
+export { SMTPClient } from "https://deno.land/x/denomailer/mod.ts";

66633d757f66ce618e909d07ac0939ffa5f7d640
André Dietrich <andredietrich@web.de>
2023-01-09 11:09:50 +0100

    fix: Catch Module loading errors
    The fetching of modules was moved into a try-catch block, such that the
    loading error can be handled and showed to the user.

--- client/plugins/mixins.js
+++ client/plugins/mixins.js
@@ -89,41 +89,49 @@ function replace(code, baseURL) {
 Vue.mixin({
     methods: {
         async scrapeModule(module) {
-            const content = (await (await fetch(module.url)).text())
-
             try {
+                const response = await fetch(module.url)
+                const content = await response.text()
+
                 if (module.url.match(/\.ya?ml$/i)) {
-                    const yaml = load(content)
+                    try {
 
-                    const links = yaml.load?.links || []
-                    const scripts = yaml.load?.scripts || []
-
-                    const code = `<!DOCTYPE html>
-                    <html>
-                    <head>
+                        const yaml = load(content)
+                        
+                        const links = yaml.load?.links || []
+                        const scripts = yaml.load?.scripts || []
+                    
+                        const code = `<!DOCTYPE html>
+                        <html>
+                        <head>
                         ${links.map((url) => { return loadResource('css', url, module.url) }).join("\n")}
+                        
+                            ${scripts.map((url) => { return loadResource('script', url, module.url) }).join("\n")}
 
-                        ${scripts.map((url) => { return loadResource('script', url, module.url) }).join("\n")}
+                            <style type="module">${yaml.style || ''}</style>
+                            <script>${ yaml.main }</script>
+                            </head>
+                            <body>
+                            ${yaml.body || ''}
+                            </body>
+                            </html>
+                            `
+                            
+                            return {
+                                ...module,
+                                name: yaml.name,
+                                description: yaml.description,
+                                icon: yaml.icon || 'mdi-package',
+                                shownIn: yaml['show-in'] || ['*'],
+                                srcdoc: "data:text/html," + escape(code),
+                                origin: '*'
+                            }
+                        } catch (error) {
+                            console.warn("loading yaml:", error)
 
-                        <style type="module">${yaml.style || ''}</style>
-                        <script>${ yaml.main }</script>
-                    </head>
-                    <body>
-                        ${yaml.body || ''}
-                    </body>
-                    </html>
-                    `
-
-                    return {
-                        ...module,
-                        name: yaml.name,
-                        description: yaml.description,
-                        icon: yaml.icon || 'mdi-package',
-                        shownIn: yaml['show-in'] || ['*'],
-                        srcdoc: "data:text/html," + escape(code),
-                        origin: '*'
-                    }
-                } else {
+                            throw new Error('Could not load the YAML-declaration: '+ error.message);
+                        }
+                    } else {
                     const moduleEl = document.createElement('html')
                     moduleEl.innerHTML = content
                     const meta = Object.fromEntries(Object.values(moduleEl.getElementsByTagName('meta')).map(m => ([m.name, m.content])))
@@ -140,12 +148,16 @@ Vue.mixin({
                         }
                     }
 
-                    return {
-                        ...module,
-                        name: moduleEl.getElementsByTagName("title")[0].innerText || meta['name'],
-                        description: meta['description'],
-                        icon: meta['icon'] || 'mdi-package',
-                        shownIn: (meta['show-in'] || '*').replaceAll(' ', '').split(',') // or 'station'
+                    try {
+                        return {
+                            ...module,
+                            name: moduleEl.getElementsByTagName("title")[0].innerText || meta['name'],
+                            description: meta['description'],
+                            icon: meta['icon'] || 'mdi-package',
+                            shownIn: (meta['show-in'] || '*').replaceAll(' ', '').split(',') // or 'station'
+                        }
+                    } catch (error) {
+                        throw new Error('This does not seem to be a valid module declaration, check the URL manually.');
                     }
                 }
             } catch (error) {
